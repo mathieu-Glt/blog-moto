1cb9abb3753b59ae2204d908411d78fa6275fa87
mathieu <mathieuglt1@gmail.com>
2022-05-04 14:19:06 +0200

    form

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const flash = require('connect-flash');
-const { body, validationResult } = require('express-validator')
+const { check, validationResult } = require('express-validator')
 
 module.exports = (app, db) =>{
 
@@ -18,16 +18,14 @@ module.exports = (app, db) =>{
 
     //route post pour enregistrer un utilisateur
     app.post('/register',
-	body('email').isEmail(),
-	body('firstName').isAlpha().isLength({min: 4}),
-	body('lastName').isAlpha().isLength({min: 4}),
-	body('password').isLength({min: 8}),
+	check('email', 'This email must correspond to a valid email ').isEmail().normalizeEmail(),
+	check('firstName', 'This firstName must to be write with aplha characters and 4 characters min').isAlpha().exists().isLength({min: 4}),
+	check('lastName', 'This lastName must to be write with aplha characters and 4 characters min').isAlpha().exists().isLength({min: 4}),
+	check('password').isLength({min: 8}),
 	 async(req, res, next)=>{
 		 const errors = validationResult(req);
 		 if(!errors.isEmpty()) {
-			 return res.status(400).json({ errors: errors.array() });
-			 //res.render('layout', {template : 'register', status: 400, msg : 'il y a eu un prblème !', result : user, session : req.sesssion})
-
+			 return res.status(400).json({ errors: errors.array()});
 		 }
         let user = await userModel.saveOneUser(req)
         console.log(user)
@@ -44,8 +42,8 @@ module.exports = (app, db) =>{
 
     //route post d'envoi du login pour la connexion (avec creation de session)
 	app.post('/login', 		
-	body('email').isEmail().normalizeEmail(),
-	body('password').isLength({ min: 4}),
+	check('email', 'This email must correspond to a valid email ').isEmail().normalizeEmail(),
+	check('password').isLength({min: 8}),
 	async(req, res, next)=>{
 		console.log(req.body)
 		let user = await userModel.getUserByEmail(req.body.email)
@@ -58,7 +56,6 @@ module.exports = (app, db) =>{
 		
 		if(user.length === 0){
 			res.render('layout', {template: 'login', error: "Email inconnu", session: req.session})
-			console.log(error)
 		}else{
 			bcrypt.compare(req.body.password, user[0].Password)
 	    	.then((same)=>{
